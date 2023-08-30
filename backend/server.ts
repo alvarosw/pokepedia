93bbb47dd677edf4db96584eefc49f8be92092e1
alvarosw <alvarossmiguel@gmail.com>
2023-08-30 15:31:31 -0300

    api: implementação da config com dotenv

--- backend/server.ts
+++ backend/server.ts
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import express from 'express';
 import routes from './src/routes/index';
 
@@ -5,7 +6,7 @@ const server = express();
 
 server.use(routes);
 
-const port = 8000;
+const port = +process.env.PORT || 8000;
 server.listen(port, () => {
-  console.debug(`Server running on: http://localhost:${port}`);
+  console.debug(`Local server running on: http://localhost:${port}`);
 });
