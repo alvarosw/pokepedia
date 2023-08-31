6d998274af7854425322886e079e46dea1d22d50
alvarosw <alvarossmiguel@gmail.com>
2023-08-31 02:18:43 -0300

    abertura de cors da api

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,11 @@
 require('dotenv').config();
 import express from 'express';
+import cors from 'cors';
 import routes from './src/routes/index';
 
 const server = express();
 
+server.use(cors());
 server.use(routes);
 
 const port = +(process.env.PORT || 8000);
